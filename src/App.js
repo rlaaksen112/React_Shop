308529a28ef687fdedc6c8f9ea8565755ce99686
KimMinJae <rlaaksen112@naver.com>
2023-04-12 23:10:15 +0900

    header 23.04.12

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./reset.css";
+import img1 from "./img/Circle.png"
+import { useState } from 'react';
 function App() {
+    let [modal,setModal] = useState(false);
     return (
         <div className="App">
-            <nav>
+                    {
+                        modal == true ? <Modal ></Modal> : null 
+                    }
+            <nav className='navbar'>
                 <div className="logo">
-                  <img src="" alt="logo" />
+                    <img src={img1} alt="logo"/>
+                    <a href="#" className='logo_title'>MinJaeKK</a>
                 </div>
-                <div className="menu-trigge">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <div className='ham-trigge'>
+                    <i class="fa-solid fa-list-ul" onClick={() => {
+                        modal == false ? setModal(true) : setModal(false)
+                    }}></i>
                 </div>
             </nav>
+
         </div>
     );
+}
+
+function Modal() {
+    return (
+        <div className="modal">
+            <div className="log">
+                <button className='login'>로그인</button>
+                <button className='sing_up'>회원가입</button>
+            </div>
+            <ul>
+                <li>남자 옷</li>
+                <li>여자 옷</li>
+                <li>공용 옷</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
